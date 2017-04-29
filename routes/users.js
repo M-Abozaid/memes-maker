67e7479479b4cb4125a979aaa9d7fec7fbc96cee
M-Abozaid <aapozaid@gmail.com>
2017-04-30 00:39:36 +0200

    hack Yay

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var htmlConvert = require('html-convert');
 /* GET users listing. */
-router.get('/mortada3.jpg', function(req, res, next) {
-
+router.get('/:id/mortada3.jpg', function(req, res, next) {
+ var sender= req.params.id
 	var convert = htmlConvert();
 
 		//var wstream = fs.createWriteStream(path.join(__dirname, '../public/mortada3.jpeg'))
 		res.writeHead(200, {
             'Content-Type': 'image/jpeg'
         });
-		convert('https://obscure-badlands-13161.herokuapp.com/render/1473221956085675', {format:'jpg', quality: 90})  
+		convert('https://obscure-badlands-13161.herokuapp.com/render/'+sender, {format:'jpg', quality: 90})  
 		  .pipe(res);
 		  
   //res.send('respond with a resource');
