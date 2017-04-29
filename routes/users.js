b780e86539df441b85b149bae8a71d2412389bd0
M-Abozaid <aapozaid@gmail.com>
2017-04-29 21:29:47 +0200

    debugging

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var htmlConvert = require('html-convert');
 /* GET users listing. */
-router.get('/mortada3.png', function(req, res, next) {
+router.get('/mortada3.jpg', function(req, res, next) {
 
 	var convert = htmlConvert();
 
 		//var wstream = fs.createWriteStream(path.join(__dirname, '../public/mortada3.jpeg'))
 
-		convert('https://obscure-badlands-13161.herokuapp.com/render/1473221956085675', {format:'png', quality: 4})  
+		convert('https://obscure-badlands-13161.herokuapp.com/render/1473221956085675', {format:'jpg', quality: 4})  
 		  .pipe(res);
 		  
   //res.send('respond with a resource');
