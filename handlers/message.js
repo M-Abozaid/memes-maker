f8435b8282b2d5073b529908d230a70eae5d9960
M-Abozaid <aapozaid@gmail.com>
2017-04-29 20:11:01 +0200

    adding html-convert first try

--- handlers/message.js
+++ handlers/message.js
@@ -2,7 +2,8 @@
 const platformHelpers = require('../platformHelpers');
 const sessionStore = require('../sessionStore');
 const GraphAPI = require('../graphAPI');
-
+var htmlConvert = require('html-convert');
+var fs = require('fs');
 
 module.exports = function handleTextMessage (sessionId, session, msg) {
 	
@@ -12,6 +13,19 @@ module.exports = function handleTextMessage (sessionId, session, msg) {
 	context.msg = msg
 	session.state = session.state  || 'new';
 
+	context.current = context.current || {}
+ 
+	var convert = htmlConvert();
 
-	
+	var wstream = fs.createWriteStream('../public/mortada3.jpeg')
+
+	convert('https://obscure-badlands-13161.herokuapp.com/1473221956085675', {format:'jpeg', quality: 50})  
+	  .pipe(wstream);
+	 
+	 wstream.on('finish', function () {
+	 	
+	  console.log('file has been written');
+	});
+
 };
+process.stdout
