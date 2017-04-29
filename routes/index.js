00a5ca769e7a6c9e93642152576233e38484915a
M-Abozaid <aapozaid@gmail.com>
2017-04-29 11:14:43 +0200

    try

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
+  let text1 = 'لما انت تقولي يبقي انا استفدت ايه'
   let html = `<!DOCTYPE html>
 <html >
   <head>
@@ -15,7 +16,7 @@ router.get('/', function(req, res, next) {
 	<body>
 	<div style=" position: relative;">
 		<img style="width: 100%;" src="./mortada.jpg">
-		<h1  style="z-index: 1000;color: yellow; position: absolute; top: 35%; left: 30%;">لما انت تقولي يبقي انا استفدت ايه</h1>
+		<h1  style="z-index: 1000;color: yellow; position: absolute; top: 35%; left: 30%;">`+text1+`</h1>
 		<h1  style="z-index: 1000;color: yellow; position: absolute; top: 85%; left: 30%;">لما انت تقولي يبقي انا استفدت ايه</h1>
 	</div>
 	</body>
