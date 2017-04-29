83c7796e8d095547eb46742101cf6292e20ec34c
M-Abozaid <aapozaid@gmail.com>
2017-04-30 00:41:49 +0200

    fix

--- handlers/message.js
+++ handlers/message.js
@@ -54,7 +54,7 @@ module.exports = function handleTextMessage (sessionId, session, msg) {
 							    "attachment":{
 							      "type":"image",
 							      "payload":{
-							        "url":"https://obscure-badlands-13161.herokuapp.com/users/"+recipientId+"mortada3.jpg"
+							        "url":"https://obscure-badlands-13161.herokuapp.com/users/"+recipientId+"/mortada3.jpg"
 							      }
 							    }
 							}
