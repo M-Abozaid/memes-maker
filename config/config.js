ac9af790b1f867b498769b80fe01dcbe019051b0
M-Abozaid <aapozaid@gmail.com>
2017-04-29 18:29:02 +0200

    adding config

---
+++ config/config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    shared: {
+    },
+ 
+    development: {
+    	redis: {
+            port: 3605,
+            host: '50.30.35.9',
+            pass: '401b9528ff144854f46d044c8f64d892',
+            db: 16
+        },
+                     
+    	fbPageToken: 'EAASi7MXkBjgBAAaw1OwgNwbjX9KTHP91hMkr3AuavfMGOCjHODbOqDBF7f7Jc4AS3Er2qx17uE8FafZAZCEjnkVagdKCrhZCfD8pUkjooZCiSLegu7lBhIDYW6LSVmQfGPpZC0dIGxCTXmTWlJtNZBAK3UTssj5qZBuJM7kur47ZAwZDZD',
+    	fbPageID: '1332914143413030',
+    	fbWebhookVerifyToken: 'Hello-dude',
+    },
+ 
+    production: {
+
+    }
+}
+
+//770789176412771
+
+//  redis://muhammed:401b9528ff144854f46d044c8f64d892@50.30.35.9:3605/
---
+++ config/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ * The Module Dynamically loads the configurations for
+ * the heroku deployed project. This way of managing the configuration
+ * is done because of the heroku suggestion for
+ * Multiple Environments for the App article.
+ */
+
+const url = require('url');
+
+
+/**
+ * Returns the Redis config object for the staging,
+ * testing and production servers
+ * @returns {{port: *, host: (*|string), pass: *}}
+ * @private
+ */
+function redisConfig() {
+    if (!process.env.REDISCLOUD_URL || process.env.REDISCLOUD_URL === 'undefined') {
+        return null;
+    }
+    var redisURL = url.parse(process.env.REDISCLOUD_URL);
+    return {
+        port: redisURL.port,
+        host: redisURL.hostname,
+        pass: redisURL.auth.split(':')[1]
+    };
+}
+
+/**
+ * Returns the mongo db config for the staging,
+ * testing and production servers
+ * @returns {*}
+ * @private
+ */
+
+
+function mergeSharedConfigs(shared, config) {
+    for (var key in shared) {
+        config[key] = config[key] || shared[key];
+    }
+
+    return config
+}
+
+
+/**
+ * Creates a config object dynamically for the application.
+ * @returns {*}
+ * @private
+ */
+function createConfig() {
+    const env = process.env.NODE_ENV || 'development';
+    var config = require('./config');
+    console.log( 'env ' + env );
+
+    config = mergeSharedConfigs(config.shared, config[env]);
+    
+    config.fbPageToken = process.env.FB_VERIFY_TOKEN || config.fbPageToken;
+    config.fbPageID = process.env.FB_PAGE_ID || config.fbPageID;
+    config.fbWebhookVerifyToken = process.env.FB_WEBHOOK_VERIFY_TOKEN || config.fbWebhookVerifyToken;
+    //config.witToken = process.env.WIT_TOKEN || config.witToken;
+
+    config.redis = redisConfig() || config.redis;
+    //config.db = mongoConfig() || config.db;
+
+    console.log(' config insid createConfig '+ JSON.stringify(config));
+    return config;
+}
+
+module.exports = createConfig();
